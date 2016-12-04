8bf09f890eeae6fe31e2082e5cce50cc90e695f9
Ted Zhu <tedz2usa@gmail.com>
2016-12-04 18:46:22 -0500

    Get canvas and context.

--- main.js
+++ main.js
@@ -2,6 +2,25 @@ var log = console.log.bind(console);
 
 window.onload = init;
 
+var ctx, canvas;
+
+
 function init() {
   log("loaded!");
+
+  canvas = document.getElementsByTagName('canvas')[0];
+  ctx = canvas.getContext('2d');
+  log(canvas);
+  log(ctx);
+
+  drawButton();
+
+}
+
+function drawButton() {
+  drawBox();
+}
+
+function drawBox() {
+
 }
